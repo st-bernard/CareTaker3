e357f48cc7165f053087323bdc20139db27ead0e
YukiIwamaTMC <0227.yuki.i@gmail.com>
2021-12-15 17:48:31 +0900

    Model,APIテンプレート作成

--- careTaker2/careTaker2/API.swift
+++ careTaker2/careTaker2/API.swift
@@ -1,2 +1,22 @@
 import Firebase
 import FirebaseDatabase
+typealias ResultHandler<T> = (Result<T, Error>) -> Void
+
+class API {
+    
+    func generateNewUser() {
+        
+    }
+    
+    func pullData() {
+        
+    }
+    
+    func updateInterval() {
+        
+    }
+    
+    func updateLastDate() {
+        
+    }
+}
--- careTaker2/careTaker2/ContentModel.swift
+++ careTaker2/careTaker2/ContentModel.swift
@@ -1,2 +1,17 @@
 import UIKit
 
+class ContentModel: Codable {
+    var name: String
+    var category: String
+    var iconImage: String
+    var interval: String
+    var lastDate: String
+    
+    init(name:String, category:String, iconImage:String, interval:String, lastDate:String){
+        self.name = name
+        self.category = category
+        self.iconImage = iconImage
+        self.interval = interval
+        self.lastDate = lastDate
+    }
+}
