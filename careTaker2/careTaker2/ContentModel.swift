9e928ee336ee6b5d13c90793e4496252c8ddd05d
YukiIwamaTMC <0227.yuki.i@gmail.com>
2021-12-16 16:29:32 +0900

    add tests

--- careTaker2/careTaker2/ContentModel.swift
+++ careTaker2/careTaker2/ContentModel.swift
@@ -16,16 +16,16 @@ class ContentModel: Codable {
         self.lastDate = lastDate
     }
     
-    func updateInterval(withInt: Int, index:Int) {
-        let id = UserDefaults.standard.string(forKey: "careTakerID")!
+    func updateInterval(withInt: Int, index:Int, idKey:String = "careTakerID") {
+        let id = UserDefaults.standard.string(forKey: idKey)!
         self.interval = withInt
         let DBRef = Database.database().reference()
         let data = try? FirebaseEncoder().encode(self)
         DBRef.child("users/\(id)/\(index)").setValue(data)
     }
     
-    func updateLastDate(withText: String, index:Int) {
-        let id = UserDefaults.standard.string(forKey: "careTakerID")!
+    func updateLastDate(withText: String, index:Int, idKey:String = "careTakerID") {
+        let id = UserDefaults.standard.string(forKey: idKey)!
         self.lastDate = withText
         let DBRef = Database.database().reference()
         let data = try? FirebaseEncoder().encode(self)
--- careTaker2/careTaker2Tests/careTaker2Tests.swift
+++ careTaker2/careTaker2Tests/careTaker2Tests.swift
@@ -7,30 +7,96 @@
 
 import XCTest
 @testable import careTaker2
+import Firebase
+import FirebaseDatabase
 
 class careTaker2Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var DBRef:DatabaseReference = Database.database().reference()
+    var id:String?
+    var model:ContentsListModel!
+    
+    
+    override func setUp() {
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        model = ContentsListModel(idKey: "testID"){ [weak self] state in
+           switch state {
+           case .loading:
+               print("----loading----")
+           case .finish:
+               XCTAssertEqual(self?.model.contents[0].name, "hair", "デフォルト値で生成される")
+               XCTAssertEqual(self?.model.contents[0].lastDate, "2021-12-01", "デフォルト値で生成される")
+               self?.id = UserDefaults.standard.string(forKey: "testID")
+               print("####setup####")
+               expectation?.fulfill()
+           case .error:
+               XCTFail("test失敗")
+               expectation?.fulfill()
+           }
         }
+        self.waitForExpectations(timeout: 1, handler: nil)
     }
+    
+    override func tearDown() {
+        //DBに保存されたデータを消す
+        
+        //UserDefaultsのIDを消す
+        UserDefaults.standard.removeObject(forKey: "testID")
+    }
+    
+    
+    func testPullData() throws {
+        print("####teststart####")
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
 
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        self.DBRef.child("users/\(id)/0/lastDate").setValue("2021-12-30")
+        model = ContentsListModel(idKey: "testID"){ [weak self] state in
+           switch state {
+           case .loading:
+               print("----loading----")
+           case .finish:
+               XCTAssertEqual(self?.model.contents[0].lastDate, "2021-12-30", "変更が反映される")
+               expectation?.fulfill()
+           case .error:
+               XCTFail("test失敗")
+               expectation?.fulfill()
+           }
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testUpdateInterval() throws {
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
+        model.contents[0].updateInterval(withInt: 100, index: 0, idKey:"testID")
+        let interval = model.contents[0].interval
+        XCTAssertEqual(interval, 100, "変更が反映されている")
+        self.DBRef.child("users/\(id)/0/interval").getData(completion: { error, data in
+            XCTAssertEqual(data.value as? Int, 100, "変更が反映されている")
+            expectation?.fulfill()
+        })
+        self.waitForExpectations(timeout: 0.5, handler: nil)
+    }
+    
+    func testUpdateLastDate() throws {
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
+        model.contents[0].updateLastDate(withText: "2021-11-30", index: 0, idKey: "testID")
+        let lastDate = model.contents[0].lastDate
+        XCTAssertEqual(lastDate, "2021-11-30", "変更が反映されている")
+        self.DBRef.child("users/\(id)/0/lastDate").getData(completion: { error, data in
+            XCTAssertEqual(data.value as? String, "2021-11-30", "変更が反映されている")
+            expectation?.fulfill()
+        })
+        self.waitForExpectations(timeout: 0.5, handler: nil)
+    }
 }
