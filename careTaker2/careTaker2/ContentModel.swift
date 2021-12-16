2edb218a514e046b23c5331d684cabb70e7fc979
YukiIwamaTMC <0227.yuki.i@gmail.com>
2021-12-16 12:57:40 +0900

    create methods (pulldata, updateInterval, & updateLastdate)

--- careTaker2/careTaker2/ContentModel.swift
+++ careTaker2/careTaker2/ContentModel.swift
@@ -1,4 +1,7 @@
 import UIKit
+import Firebase
+import FirebaseDatabase
+import CodableFirebase
 
 class ContentModel: Codable {
     var name: String
@@ -12,4 +15,22 @@ class ContentModel: Codable {
         self.interval = interval
         self.lastDate = lastDate
     }
+    
+    func updateInterval(withInt: Int, index:Int) {
+        let id = UserDefaults.standard.string(forKey: "careTakerID")!
+        self.interval = withInt
+        let DBRef = Database.database().reference()
+        let data = try? FirebaseEncoder().encode(self)
+        DBRef.child("users/\(id)/\(index)").setValue(data)
+    }
+    
+    func updateLastDate(withText: String, index:Int) {
+        let id = UserDefaults.standard.string(forKey: "careTakerID")!
+        self.lastDate = withText
+        let DBRef = Database.database().reference()
+        let data = try? FirebaseEncoder().encode(self)
+        DBRef.child("users/\(id)/\(index)").setValue(data)
+        
+    }
+    
 }
