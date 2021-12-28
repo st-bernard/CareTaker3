3feb0e8954355d4770281c16a9dc79f53ee1a0d5
mtonosaki <manabu@tomarika.com>
2021-12-28 14:29:54 +0900

    メーターDesign

--- CareTaker3/CareTaker3/UI/MyMeterCell.swift
+++ CareTaker3/CareTaker3/UI/MyMeterCell.swift
@@ -8,11 +8,58 @@
 import UIKit
 
 class MyMeterCell : UICollectionViewCell {
+    
+    // セルの値を決める
     func setValue(model: ContentModel) {
+        
+        guard let meter = contentView as? MyMeterView else {
+            fatalError("ContentViewが MyMeterViewオブジェクトを期待した設計")
+        }
+        let now = Date()
+        let lastDateCalc = DateUtils.dateFromString(string: model.lastDate+" 00:00:00 +00:00", format: "yyyy年MM月dd日 HH:mm:ss Z")
+        let elapsedDays = Calendar.current.dateComponents([.day], from: lastDateCalc, to: now).day ?? 999
+        var percent = CGFloat(elapsedDays) / CGFloat(model.interval)
+        percent = max(0, percent)
+        let adeg = max(-5, 180.0 - percent * 120.0)
+        meter.angleDeg = adeg
+        meter.title = "\(model.name)"
+        meter.labels = makeLabels(interval: model.interval)
+
 //        let label1 = contentView.viewWithTag(77001) as! UILabel
 //        label1.text = model.name
-//        
+//
 //        let label2 = contentView.viewWithTag(77002) as! UILabel
 //        label2.text = model.lastDate
     }
+    
+    func makeLabels(interval: Int) -> [MyMeterView.LabelItem] {
+        switch interval {
+            case 40: return [
+                MyMeterView.LabelItem(text: "40", angleDeg: 182, offset: .zero),
+                MyMeterView.LabelItem(text: "30", angleDeg: 150, offset: CGPoint(x: 0, y: 1)),
+                MyMeterView.LabelItem(text: "20", angleDeg: 120, offset: CGPoint(x: -1, y: 2)),
+                MyMeterView.LabelItem(text: "10", angleDeg: 90, offset: CGPoint(x: -2, y: 3)),
+                MyMeterView.LabelItem(text: "0", angleDeg: 60, offset: CGPoint(x: -1, y: 4))
+            ]
+            case 30: return [
+                MyMeterView.LabelItem(text: "30", angleDeg: 182, offset: CGPoint(x: 0, y: 1)),
+                MyMeterView.LabelItem(text: "20", angleDeg: 140, offset: CGPoint(x: 0, y: 3)),
+                MyMeterView.LabelItem(text: "10", angleDeg: 100, offset: CGPoint(x: -2, y: 4)),
+                MyMeterView.LabelItem(text: "0", angleDeg: 60, offset: CGPoint(x: -1, y: 5))
+            ]
+            case 10: return [
+                MyMeterView.LabelItem(text: "10", angleDeg: 182, offset: .zero),
+                MyMeterView.LabelItem(text: "5", angleDeg: 120, offset: CGPoint(x: 1, y: 4)),
+                MyMeterView.LabelItem(text: "0", angleDeg: 60, offset: CGPoint(x: -1, y: 5))
+            ]
+            case 2:
+                return [
+                    MyMeterView.LabelItem(text: "2", angleDeg: 182, offset: .zero),
+                    MyMeterView.LabelItem(text: "1", angleDeg: 120, offset: CGPoint(x: 1, y: 3)),
+                    MyMeterView.LabelItem(text: "0", angleDeg: 60, offset: CGPoint(x: -1, y: 6))
+                ]
+            default: return []
+        }
+    }
+
 }
--- CareTaker3/CareTaker3/UI/MyMeterView.swift
+++ CareTaker3/CareTaker3/UI/MyMeterView.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class MyMeterView : UIView {
     
+    struct LabelItem {
+        var text: String
+        var angleDeg: CGFloat
+        var offset: CGPoint
+    }
+    
     var angleDeg: CGFloat = 45
+    var labels = Array<LabelItem>()
+    var title: String = "Hoge"
     
     override func draw(_ rect: CGRect) {
 
@@ -18,10 +26,26 @@ class MyMeterView : UIView {
         let H = self.frame.size.height
         let O = CGPoint(x: W / 2, y: W * 0.442) // 針の根本の座標
         let R = (H - (H - O.y) * 2)
-        let Rx = 1.15
+        let Rx: CGFloat = 1.15
+        let Rlabel = R * 0.75
         
         // メーターの画像をセルとピッタリのサイズで描画
         UIImage(named: "Meter")?.draw(in: CGRect(origin: .zero, size: CGSize(width: W, height: H)))
+        
+        // ラベル描画
+        for label in labels {
+            let r0 = Rlabel + (Rx * abs(cos(label.angleDeg)))
+            let x = cos(label.angleDeg * Pi / 180.0) * r0 + O.x + label.offset.x
+            let y = -sin(label.angleDeg * Pi / 180.0) * r0 + O.y + label.offset.y
+            label.text.draw(
+                at: CGPoint(x: x, y: y),
+                withAttributes: [
+                    NSAttributedString.Key.foregroundColor : UIColor(red: 0.513725490196078, green: 0.686274509803922, blue: 0.694117647058824, alpha: 1),
+                    NSAttributedString.Key.font : UIFont.systemFont(ofSize: 6),
+                ]
+            )
+
+        }
         
         // メーターの針を描画
         let line = UIBezierPath();
@@ -32,5 +56,14 @@ class MyMeterView : UIView {
         UIColor.red.setStroke()
         line.lineWidth = 2
         line.stroke();
+        
+        // タイトル表示
+        title.draw(
+            at: CGPoint(x: 3, y: 3),
+            withAttributes: [
+                NSAttributedString.Key.foregroundColor : UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1),
+                NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12),
+            ]
+        )
     }
 }
