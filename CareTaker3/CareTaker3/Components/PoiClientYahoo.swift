0b609d43d715f8fe436a2714fe5e9f9d28b7bb5d
mtonosaki <manabu@tomarika.com>
2021-12-29 11:54:25 +0900

    add Yahoo POI Client

---
+++ CareTaker3/CareTaker3/Components/PoiClientYahoo.swift
@@ -0,0 +1,8 @@
+//
+//  PoiClient.swift
+//  CareTaker3
+//
+//  Created by Manabu Tonosaki on 2021/12/29.
+//
+
+import Foundation
---
+++ CareTaker3/CareTaker3Tests/YahooPoiClientTests.swift
@@ -0,0 +1,106 @@
+//
+//  careTaker2Tests.swift
+//  careTaker2Tests
+//
+//  Created by Yuki Iwama on 2021/12/15.
+//
+
+import XCTest
+@testable import CareTaker3
+import Firebase
+import FirebaseDatabase
+
+class CareTaker3Tests: XCTestCase {
+    var DBRef:DatabaseReference = Database.database().reference()
+    var id:String?
+    var model:ContentsListModel!
+    
+    
+    override func setUp() {
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        model = ContentsListModel(idKey: "testID")
+        model.configuration() { [weak self] state in
+           switch state {
+           case .loading:
+               print("----loading----")
+           case .finish:
+               XCTAssertEqual(self?.model.contents[0][0].name, "hair", "デフォルト値で生成される")
+               XCTAssertEqual(self?.model.contents[0][0].lastDate, "2021年12月01日", "デフォルト値で生成される")
+               self?.id = UserDefaults.standard.string(forKey: "testID")
+               print("####setup####")
+               expectation?.fulfill()
+           case .error:
+               XCTFail("test失敗")
+               expectation?.fulfill()
+           }
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    override func tearDown() {
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
+        //DBに保存されたデータを消す
+        self.DBRef.child("users/\(id)").removeValue()
+        //UserDefaultsのIDを消す
+        UserDefaults.standard.removeObject(forKey: "testID")
+    }
+    
+    
+    func testPullData() throws {
+        print("####teststart####")
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
+
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        self.DBRef.child("users/\(id)/0/0/lastDate").setValue("2021年12月30日")
+        model = ContentsListModel(idKey: "testID")
+        model.configuration() { [weak self] state in
+           switch state {
+           case .loading:
+               print("----loading----")
+           case .finish:
+               XCTAssertEqual(self?.model.contents[0][0].lastDate, "2021年12月30日", "変更が反映される")
+               expectation?.fulfill()
+           case .error:
+               XCTFail("test失敗")
+               expectation?.fulfill()
+           }
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testUpdateInterval() throws {
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
+        let updateFirebase = UpdateFirebase(section: 0, row: 0)
+        updateFirebase.updateInterval(withInt: 100, idKey:"testID")
+        self.DBRef.child("users/\(id)/0/0/interval").getData(completion: { error, data in
+            XCTAssertEqual(data.value as? Int, 100, "変更が反映されている")
+            expectation?.fulfill()
+        })
+        self.waitForExpectations(timeout: 0.5, handler: nil)
+    }
+    
+    func testUpdateLastDate() throws {
+        let expectation: XCTestExpectation? = self.expectation(description: "sample")
+        guard let id = self.id else {
+            XCTFail("UseDefaultsにIDがなかったら失敗")
+            return
+        }
+        let updateFirebase = UpdateFirebase(section: 0, row: 0)
+        updateFirebase.updateLastDate(withText: "2021年11月30日", idKey: "testID")
+        self.DBRef.child("users/\(id)/0/0/lastDate").getData(completion: { error, data in
+            XCTAssertEqual(data.value as? String, "2021年11月30日", "変更が反映されている")
+            expectation?.fulfill()
+        })
+        self.waitForExpectations(timeout: 0.5, handler: nil)
+    }
+}
