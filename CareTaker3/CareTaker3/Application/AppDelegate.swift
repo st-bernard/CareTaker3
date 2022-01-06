f525baf3c4574f81054181c08e4342eaadb44005
mtonosaki <manabu@tomarika.com>
2022-01-06 17:04:09 +0900

    期限通知機能

--- CareTaker3/CareTaker3/Application/AppDelegate.swift
+++ CareTaker3/CareTaker3/Application/AppDelegate.swift
@@ -9,16 +9,16 @@ import UIKit
 import Firebase
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.rootViewController = CollectionViewController()
-//        window?.makeKeyAndVisible()
+
+        initPushNotification()
+        
         return true
     }
 
@@ -35,7 +35,59 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func initPushNotification() {
+        UNUserNotificationCenter.current().requestAuthorization(    // PUSH通知の受信機能 有効化1
+            options: [.alert, .sound, .badge]){
+            (granted, _) in
+            if granted {
+                UNUserNotificationCenter.current().delegate = self
+                
+            } else {
+                print("ERROR: PUSH Authorization error")
+            }
+        }
+    }
+    
+    // PUSH通知対応
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        print("------ didReceiveRemoteNotification 明示通知")
+    }
+    
+    // サイレント通知対応
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("------ didReceiveRemoteNotification サイレント通知")
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
 
+    // HACK:アプリがフォアグランドにいるときにPUSHされたイベント処理
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
 
+        let userInfo = notification.request.content.userInfo
+        print("======= PUSH1 \(userInfo)")
+        completionHandler([ .badge, .sound, .banner ])
+   }
+
+    // HACK:ユーザーが通知バナーをタップした時に発火するイベント処理
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print("======= PUSH2 \(userInfo)")
+        completionHandler()
+    }
+    
+    // トークン取得完了時のイベント
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken token: String?) {
+
+        print("------- Notification token: \(String(describing: token))")
+
+        let dataDict: [String: String] = ["token": token ?? ""]
+        NotificationCenter.default.post(
+          name: Notification.Name("FCMToken"),
+          object: nil,
+          userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+      }
 }
 
